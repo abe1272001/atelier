dcbc710f3b5f26a18e2c935590010fb48b26c8d0
Abe Chen <abe1272001@gmail.com>
2022-05-07 12:07:50 +0800

    home page basic design

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,131 @@
 import Head from 'next/head';
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+import {
+	Button,
+	useColorMode,
+	Link,
+	Box,
+	Grid,
+	GridItem,
+	VStack,
+	Image,
+	Text,
+	useColorModeValue,
+	Heading,
+} from '@chakra-ui/react';
+
+// {
+// 	iiii_atelier;
+// }
+// Parfume;
+// Aroma / Scent;
+// Hand;
+// Body;
+
+// {
+// 	FORi;
+// }
+// Tableware;
+// Deco;
+// Vintages;
 
 export default function Home() {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const categories = [
+		{
+			categoryName: 'Aroma/Scent',
+			imageUrl: '/images/S__14245957.jpg',
+		},
+		{
+			categoryName: 'Parfume',
+			imageUrl: '/images/S__14245962.jpg',
+		},
+		{
+			categoryName: 'Hand',
+			imageUrl: '/images/S__14245935.jpg',
+		},
+		{
+			categoryName: 'Body',
+			imageUrl: '/images/S__14245937.jpg',
+		},
+		{
+			categoryName: 'Tableware',
+			imageUrl: '/images/S__14245959.jpg',
+		},
+		{
+			categoryName: 'Deco',
+			imageUrl: '/images/S__14245938.jpg',
+		},
+		{
+			categoryName: 'Vintages',
+			imageUrl: '/images/S__14245962.jpg',
+		},
+	];
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+		<VStack spacing={4} align="stretch">
 			<Head>
-				<title>Create Next App</title>
+				<title>atelier</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<Box
+				as="main"
+				mt={3}
+				// border="1px"
+				// borderColor="gray.200"
+				// height="1000px"
+				// maxHeight="1200px"
+			>
+				<Box
+					width="100%"
+					bg="#616C6E"
+					height={['200px', '380px']}
+					// overflow="hidden"
+					backgroundImage="url('/images/logo.jpg')"
+					backgroundPosition="50% 50%"
+					backgroundRepeat="no-repeat"
+					backgroundSize="86%"
+				/>
+				<Box>
+					<Grid templateColumns="repeat(2, 1fr)" gap={0}>
+						{categories.map((category) => (
+							<NextLink
+								key={category.categoryName}
+								href={`/collection/${category.categoryName}`}
+							>
+								<Link>
+									<GridItem width="100%" bg="gray.100" position="relative">
+										<Image
+											src={category.imageUrl}
+											fit="cover"
+											width="100%"
+											height={['200px', '380px']}
+											alt={category.categoryName}
+										/>
+										<Text
+											textAlign="center"
+											position="absolute"
+											bottom="8px"
+											left="8px"
+											px={3}
+											py={1}
+											borderRadius="base"
+											bg={'whiteAlpha.800'}
+											css={{ backdropFilter: 'blur(8px)' }}
+										>
+											{category.categoryName}
+										</Text>
+									</GridItem>
+								</Link>
+							</NextLink>
+						))}
+					</Grid>
+				</Box>
+			</Box>
 
-			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-				<h1 className="text-6xl font-bold bg-red-500">
-					Welcome to{' '}
-					<a className="text-blue-600" href="https://nextjs.org">
-						Next.js!
-					</a>
-				</h1>
-
-				<p className="mt-3 text-2xl">
-					Get started by editing{' '}
-					<code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-						pages/index.js
-					</code>
-				</p>
-
-				<div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-					<a
-						href="https://nextjs.org/docs"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Find in-depth information about Next.js features and API.
-						</p>
-					</a>
-
-					<a
-						href="https://nextjs.org/learn"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Learn &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Learn about Next.js in an interactive course with quizzes!
-						</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Examples &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Discover and deploy boilerplate example Next.js projects.
-						</p>
-					</a>
-
-					<a
-						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
-			</main>
-
-			<footer className="flex items-center justify-center w-full h-24 border-t">
-				<a
-					className="flex items-center justify-center"
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-				</a>
-			</footer>
-		</div>
+			{/* <Button onClick={toggleColorMode}>
+				Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+			</Button> */}
+		</VStack>
 	);
 }
