12e64d3d849a8bc27c2babb4ab48ea1987985460
Abe Chen <abe1272001@gmail.com>
2022-05-07 12:07:17 +0800

    setup theme and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
+import theme from '../theme';
+import Fonts from '../styles/fonts.js';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
 	return (
-		<ChakraProvider>
-			<Layout>
+		<ChakraProvider theme={theme}>
+			<Fonts />
+			<Layout router={router}>
 				<Component {...pageProps} />
 			</Layout>
 		</ChakraProvider>
--- theme/index.js
+++ theme/index.js
@@ -2,6 +2,16 @@ import { extendTheme } from '@chakra-ui/react';
 
 // 2. Add your color mode config
 const config = {
+	colors: {
+		brand: {
+			100: '#6E7C7E',
+			300: '#A4B0B1',
+		},
+	},
+	fonts: {
+		heading: 'Heading Font Name, sans-serif',
+		body: 'Body Font Name, sans-serif',
+	},
 	initialColorMode: 'light',
 	useSystemColorMode: false,
 };
