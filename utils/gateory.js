a309f5b950c49e9a1debbcbe281b59c7eb4c73d1
Abe Chen <abe1272001@gmail.com>
2022-05-09 12:04:10 +0800

    extract fake gategory

---
+++ utils/gateory.js
@@ -0,0 +1,30 @@
+const fakeCategories = [
+	{
+		name: 'Aroma/Scent',
+		image: '/images/S__14245957.jpg',
+	},
+	{
+		name: 'Parfume',
+		image: '/images/S__14245962.jpg',
+	},
+	{
+		name: 'Hand',
+		image: '/images/S__14245935.jpg',
+	},
+	{
+		name: 'Body',
+		image: '/images/S__14245937.jpg',
+	},
+	{
+		name: 'Tableware',
+		image: '/images/S__14245959.jpg',
+	},
+	{
+		name: 'Deco',
+		image: '/images/S__14245938.jpg',
+	},
+	{
+		name: 'Vintages',
+		image: '/images/S__14245962.jpg',
+	},
+];
